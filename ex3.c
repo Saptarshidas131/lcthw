75e5150adf15445078ee11a430525948f254da7b
saptarshidas131 <saptarshidas131@gmail.com>
2021-12-18 13:41:45 +0530

    added comments

--- ex3.c
+++ ex3.c
@@ -1,12 +1,18 @@
+// include the stdio header to perform standard input/output functions, to use printf here
 #include <stdio.h>
 
+// main function with integer return type
 int main()
 {
+    // assign 10 to integer vriable age 
 	int age = 10;
+    // assign 72 to height
 	int height = 72;
 
+    // print age and height using formatted string %d is replaced by the varible value
 	printf("I am %d years old.\n",age);
 	printf("I am %d inches tall.\n",height);
 
+    // return 0 to OS to successfup program exit
 	return 0;
 }
