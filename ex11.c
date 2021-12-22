558e9a00c421d3b32501c2765c708d0e7fd2d9bc
saptarshidas131 <saptarshidas131@gmail.com>
2021-12-22 14:04:02 +0530

    added ex11 ex14 and ex14a

--- ex11.c
+++ ex11.c
@@ -2,21 +2,28 @@
 
 int main(int argc, char *argv[])
 {
+    // initialize the first element with 0 and the rest of array is filled with 0
 	int numbers[4] = {0};
+    // initialize the first element with 'a' and the rest of array is filled with 0
 	char name[4] = {'a'};
 
 	// first, print them out raw
+    // print the numbers array element by element
 	printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+    // print the name array element by element
 	printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
+    // print name
 	printf("name: %s\n", name);
 
 	// setup the numbers
+    // assign each integer to the array index separately
 	numbers[0] = 1;
 	numbers[1] = 2;
 	numbers[2] = 3;
 	numbers[3] = 4;
 
 	// setup the name
+    // assign each character to the name array index seperately followed by the null character
 	name[0] = 'S';
 	name[1] = 'd';
 	name[2] = 'a';
@@ -31,10 +38,13 @@ int main(int argc, char *argv[])
 	printf("name: %s\n", name);
 
 	// another way to use name
+    // use character pointer to intialize a character array
 	char *another = "SDas";
 
+    // print the array
 	printf("another: %s\n", another);
 
+    // print the array element wise
 	printf("another each: %c %c %c %c\n", another[0], another[1], another[2], another[3]);
 
 	return 0;
--- ex14a.c
+++ ex14a.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 // include ctype header for isalpha and isblank
 #include <ctype.h>
+// include string header for strlen function
+#include <string.h>
 
 // forward declarations
 // declare the functions before use
 int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char arg[], int slength);
 
 // print_arguments function takes two arguments which are command line arguments to main
 void print_arguments(int argc, char *argv[])
@@ -15,17 +17,18 @@ void print_arguments(int argc, char *argv[])
 	
     // go through all the arguments call print_letters on it
 	for(i = 0; i < argc; i++) {
-		print_letters(argv[i]);
+        int slen = strlen(argv[i]);
+		print_letters(argv[i], slen);
 	}
 }
 
 // print_letters takes one argument and  prints ASCII code of it if it is a alphabet of blank 
-void print_letters(char arg[])
+void print_letters(char arg[], int slength)
 {
 	int i = 0;
 
-    // go through the argument as long as it does not contains the NULL or ends
-	for(i = 0; arg[i] != '\0'; i++) {
+    // go through the argument till the length of the argument
+	for(i = 0; i < slength; i++) {
         // assign each character of the argument to a character variable ch
 		char ch = arg[i];
 
