fbe3ce068ada3742a14ad03441f5176b30713bed
saptarshidas131 <saptarshidas131@gmail.com>
2022-04-12 09:25:30 +0530

    modified ex7.c

--- ex7.c
+++ ex7.c
@@ -1,14 +1,18 @@
-// import standard input/output header i/o operations
+// import standard input/output header for i/o operations
 #include <stdio.h>
 
 // main function
 int main(int argc, char *argv[])
 {
     // variable declaration and assignment
+    // assign 100 to distance
 	int  distance = 100;
+    // assign 2.345f to power
 	float  power = 2.345f;
+    // assign the large value to super_power
 	double super_power = 566789.4532;
-	char initial = 'S';
+    // assign initial the character S
+	char initial = ' ';
     // assign the variable the character array
 	char first_name[] = "Saptarshi";
 	char last_name[] = "Das";
@@ -17,8 +21,10 @@ int main(int argc, char *argv[])
 	printf("You are %d miles away.\n", distance);
 	printf("You have %f levels of power.\n", power);
 	printf("You have %f awesome super powers.\n",super_power);
+    // %c is used to print character
 	printf("I have an initial %c.\n", initial);
 	printf("I have a first name %s.\n", first_name);
+    // %s is used to print string or characte array
 	printf("I have a last name %s.\n",last_name);
 	printf("My whole name is %s %c.  %s.\n", first_name, initial, last_name);
 
@@ -31,7 +37,7 @@ int main(int argc, char *argv[])
 
     // assign some large data with the long type modifier
 	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
-    // print it out
+    // print it out, ld is used to print long decimal
 	printf("The entire universe has %ld bugs. \n", universe_of_defects);
 
     // do some calculation and print it on next line
@@ -40,7 +46,7 @@ int main(int argc, char *argv[])
 
     // calculate the rate of bugs compared to bugs in universe
 	double part_of_universe = expected_bugs / universe_of_defects;
-    // print it
+    // print it, %e is used to print exponential data
 	printf("That is only a %e portion of the universe.\n", part_of_universe);
 
 	// this make no sense, just a demo of something weird
@@ -51,6 +57,7 @@ int main(int argc, char *argv[])
     // print it, %% is used to print %
 	printf("Which means you should care %d%%. \n", care_percentage);
 
+    // return 0 from main
 	return 0;
 
 }
